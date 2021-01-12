4e05dd231f7b654c3a815f13097b2ef3b50e4928
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-01-12 20:40:57 +0530

    Update armstrongNum.py

--- Basic Programs/armstrongNum.py
+++ Basic Programs/armstrongNum.py
@@ -1,1 +1,11 @@
-
+n=int(input("Enter any number :"));
+cube=0;
+temp=n;
+while(n>0):
+	no=n%10
+	cube=cube+(no**3)
+	n=n//10
+if(temp==cube):
+	print("No is armstrong")
+else:
+	print("No is not armstrong")
